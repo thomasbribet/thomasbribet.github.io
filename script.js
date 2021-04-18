4f2edbc51011beedc1fc73578d161cb3a1626ea0
Thomas <thomas.bribet@gmail.com>
2021-04-19 01:12:27 +0200

    incrementation heures competences en js done

--- script.js
+++ script.js
@@ -36,8 +36,7 @@ dropdown('#rigueur', '#rigueur div');
 
 // INCREMENTATION COMPETENCES 
 
-
-var hoursOfPractice = function(langId, start, incrementInHours) { 
+var hoursOfPractice = function(langId, start, incrementInHours) { // incrementInHours = hours of practice add each week
     var badge = document.getElementById(langId);
     var startDate = new Date(start);
     var currentDate = new Date();
@@ -46,29 +45,26 @@ var hoursOfPractice = function(langId, start, incrementInHours) {
     var intervalInHours = (interval/(3.6e+6)).toFixed(2); // number of hours since dateStart
     var intervalInWeeks = (intervalInHours/24)/7;
 
-    var totalHours = Math.round(intervalInWeeks*incrementInHours) + " h"; // number of hours add each week since dateStart (estimation)
+    var totalHours = Math.round(intervalInWeeks*incrementInHours); // number of practice hours add each week since dateStart (estimation)
+    badge.innerHTML = totalHours + " h";
 
-    if(totalHours<200) {
-        badge.innerHTML = totalHours;
+    if(totalHours<200) { // débutant jusqu'à 200h
         badge.classList.add('bg-success');
     }
-    else if(totalHours>200 && totalHours<5000) {
-        badge.innerHTML = totalHours;
-        badge.classList.add("bg-primary");
+    else if(totalHours>200 && totalHours<5000) { // intermédiare jusqu'à 5.000h
+        badge.classList.add('bg-primary')
     }
-    else if(totalHours>5000 && totalHours<10000) {
-        badge.innerHTML = totalHours;
-        badge.classList.add('bg-danger');
+    else if(totalHours>5000 && totalHours<10000) { // confirmé jusqu'à 10.000h
+        badge.classList.add('bg-danger')
     }
-    else if(totalHours>10000) {
-        badge.innerHTML = totalHours;
-        badge.classList.add('bg-dark');
+    else if(totalHours>10000) { // expert au-delà 10.000h
+        badge.classList.add('bg-dark')
     }
-    else {
-        badge.innerHTML = totalHours;
-        badge.classList.add('bg-warning');
-
-    console.log(badge);
+   
 }
 
-
+hoursOfPractice('html', '2020/03/01', 5); 
+hoursOfPractice('css', '2020/03/08', 5);
+hoursOfPractice('js', '2021/03/01', 8);
+hoursOfPractice('sql', '2020/04/01', 1);
+hoursOfPractice('php', '2020/04/01', 0.5);
