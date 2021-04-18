f83a460e021f7704187e5d50a99c4049843115b7
Thomas <thomas.bribet@gmail.com>
2021-04-19 00:46:38 +0200

    fonction increment heures start

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // HIGHLIGHT CURRENT PAGE 
 
 var highlight = function() {
-    var navPage = document.querySelectorAll("#classicMenu div a");
+    var navPage = document.querySelectorAll("nav .page");
     var currentLocation = location.hash.substring(1);
     for(let i = 0; i<navPage.length; i++) {
         if(navPage[i].innerHTML === currentLocation.toUpperCase()) {
@@ -33,3 +33,42 @@ dropdown('#adapt', '#adapt div');
 dropdown('#rigueur', '#rigueur div');
 
 ////////////////////////////////////////////////////////////////////////
+
+// INCREMENTATION COMPETENCES 
+
+
+var hoursOfPractice = function(langId, start, incrementInHours) { 
+    var badge = document.getElementById(langId);
+    var startDate = new Date(start);
+    var currentDate = new Date();
+
+    var interval = currentDate.getTime() - startDate.getTime();
+    var intervalInHours = (interval/(3.6e+6)).toFixed(2); // number of hours since dateStart
+    var intervalInWeeks = (intervalInHours/24)/7;
+
+    var totalHours = Math.round(intervalInWeeks*incrementInHours) + " h"; // number of hours add each week since dateStart (estimation)
+
+    if(totalHours<200) {
+        badge.innerHTML = totalHours;
+        badge.classList.add('bg-success');
+    }
+    else if(totalHours>200 && totalHours<5000) {
+        badge.innerHTML = totalHours;
+        badge.classList.add("bg-primary");
+    }
+    else if(totalHours>5000 && totalHours<10000) {
+        badge.innerHTML = totalHours;
+        badge.classList.add('bg-danger');
+    }
+    else if(totalHours>10000) {
+        badge.innerHTML = totalHours;
+        badge.classList.add('bg-dark');
+    }
+    else {
+        badge.innerHTML = totalHours;
+        badge.classList.add('bg-warning');
+
+    console.log(badge);
+}
+
+
