fc134c124b865ff7b5773a33f262b4f346fc46e0
Thomas <thomas.bribet@gmail.com>
2021-05-06 22:10:13 +0200

    fin contact

--- js/contact.js
+++ js/contact.js
@@ -4,4 +4,25 @@
 // }
 
 // message();
-
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+          status.classList.add("success");
+        status.innerHTML = "Merci, je vous recontacte au plus vite!";
+        form.reset()
+      }).catch(error => {
+        status.classList.add("error");
+        status.innerHTML = "Mince alors! une erreur..."
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
